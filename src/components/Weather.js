6454e575ffdfc14443db003570ebaff08feb5d6e
BytesHouse <a.lihtgolt@gmail.com>
2023-09-07 19:37:19 +0300

    tbc

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,6 @@
 export const Weather = ({weather}) =>{
     const {city, country, temp, pressure, sunset} = weather;
-    console.log(weather)
+    console.log(country)
     return (
         <div className="infoWeath">
             <p>Location: {country} {city}</p>
